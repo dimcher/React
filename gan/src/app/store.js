dffa15a72822b064485514e0ef2df7a0ef6991d3
Dima Chernyshev <dimcher@gmail.com>
2021-09-04 16:12:40 -0400

    reducer added

--- gan/src/app/store.js
+++ gan/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import reducer from './reducer';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    dummy: reducer
   },
 });
+
+export default store; 
