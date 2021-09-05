33341edcba76a785c371b158c0d75b55daf02fa4
Dima Chernyshev <dimcher@gmail.com>
2021-09-05 10:45:06 -0400

    test

--- gan/src/App.js
+++ gan/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { loadCatalog } from './app/actions';
+import { loadList } from './app/actions';
 import { useDispatch } from 'react-redux';
 
 import './App.css';
@@ -8,13 +8,13 @@ import {Header} from './components/Header'
 import Slots from './components/Slots'
 
 function App () {
-  const dispatch = useDispatch();
-
+/*  const dispatch = useDispatch();
   const loadData = function () {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(res => res.json())
-      .then(data => data);
-}
+      .then(data => dispatch(loadList(data))
+      );
+}*/
 
   return (
     <div className="App">
--- gan/src/components/Views.js
+++ gan/src/components/Views.js
@@ -1,18 +1,22 @@
-import React,  {Component} from 'react';
-import { useSelector } from 'react-redux';
+import React  from 'react';
+import { changeView } from '../app/actions';
+import { useSelector, useDispatch } from 'react-redux';
 
 import './Styles.css';
 
 const Views = () => {
     const view = useSelector(state => state.view.view);
     const views = useSelector(state => state.view.views);
+    const dispatch = useDispatch();
+    const changeViewHandler = (idx) => {
+        dispatch(changeView(idx));
+    };
     const code = views.map((item, idx) => {
-        console.log(view);
         const i = idx === view
         ? <p className="active">{item.name}</p>
         : <p>{item.name}</p>;
 
-        return <div key={idx}>{i}</div>
+        return <div key={idx} onClick={() => changeViewHandler(idx)}>{i}</div>
     }); 
     return (
         <div className="views">{code}</div>    
