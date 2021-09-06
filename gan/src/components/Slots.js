c65104a9c6e80a8b17e49517826e122ca970688d
Dima Chernyshev <dimcher@gmail.com>
2021-09-06 09:19:15 -0400

    test

--- gan/src/components/Slots.js
+++ gan/src/components/Slots.js
@@ -8,7 +8,10 @@ const Slots = () => {
     
     const slots = useSelector(state => state.list.show);
     const code = slots.map((slot, idx) => {
-        return <div key={idx}><img src={'https://www.themoviedb.org/t/p/w440_and_h660_face' + slot.poster_path} alt="" /></div>
+        const src = 'https://www.themoviedb.org/t/p/w440_and_h660_face' + slot.poster_path;
+        return <div key={idx}>
+                    <img src={ src } alt="" />
+                </div>
     });
     return (
         <div className="slots">
